8ca69692dd1b2efe2b6c45e8a22aef625d1c0a72
kl1ko <klimjudo@gmail.com>
2024-04-30 20:38:56 +0300

    first commit

---
+++ trainee-task/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RandomIconButton from "./RandomIconButton";
+
+function App() {
+  return (
+    <div className="App">
+      <RandomIconButton />
+    </div>
+  );
+}
+
+export default App;
